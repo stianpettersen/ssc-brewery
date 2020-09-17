5ff9145c1aa161df80877abee8f29fba44abaf62
Stian <stian.pettersen@icloud.com>
2020-09-17 13:53:25 +0200

    Addeed SecurityConfig and permit login + find beers

---
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package guru.sfg.brewery.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+                http
+                        .authorizeRequests(authorize -> {
+                            authorize.antMatchers("/",
+                                            "/login",
+                                            "/beers/find",
+                                            "/webjars/**",
+                                            "/resources/**")
+                                    .permitAll();
+                        })
+                        .authorizeRequests()
+                        .anyRequest().authenticated()
+                        .and()
+                        .formLogin()
+                        .and()
+                        .httpBasic();
+    }
+}
