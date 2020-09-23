93077998ce207c195d77d6c5e946aa5e9d4496a4
Stian <sp@computas.com>
2020-09-23 07:19:12 +0200

    Added Lombok config to User and Authority

--- src/main/java/guru/sfg/brewery/domain/security/Authority.java
+++ src/main/java/guru/sfg/brewery/domain/security/Authority.java
@@ -1,8 +1,15 @@
 package guru.sfg.brewery.domain.security;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.Set;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 public class Authority {
 
